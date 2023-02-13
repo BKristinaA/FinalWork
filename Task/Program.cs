19e378da75367963ccd1d1be378e2bae195ccffd
Christina <bihtya@mail.ru>
2023-02-13 12:08:02 +0300

    Второй метод - ввод длины строки в массиве

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,3 +3,8 @@
   Console.Write("\nВведите текст через пробел: ");
   return Console.ReadLine()!.Split(" ");
 }
+int CountElementInStringArray(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
