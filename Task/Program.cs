3fa441a1aefa52d4e13883253acad6a7a5a481a7
Christina <bihtya@mail.ru>
2023-02-13 12:14:37 +0300

    Третий метод - количество элементов в новом массиве

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,8 +3,22 @@
   Console.Write("\nВведите текст через пробел: ");
   return Console.ReadLine()!.Split(" ");
 }
+
 int CountElementInStringArray(string text)
 {
     Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountElemetNewArray(string[] collection, int countSymbol) 
+{
+    int count = 0;
+    for(int i = 0; i < collection.Length; i++) 
+    {
+        if(collection[i].Length <= countSymbol) 
+        {
+            count++;
+        }
+    }
+    return count;
 }
